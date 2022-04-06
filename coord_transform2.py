5b310b5bade28ee514a849c3c08d45d0afabca89
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2022-04-06 14:38:15 +0100

    Add extra modes to change_basis function
    Extra modes allow construction of change of basis  matrix from x,z and
    y,z combinations of vectors.

--- coord_transform2.py
+++ coord_transform2.py
@@ -1,4 +1,5 @@
 # %%
+from ast import Raise
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -24,29 +25,34 @@ def plane_fitter(point_coords):
     return norm, centroid
 
 
-def change_basis(x_new, y_new):
+def change_basis(v1, v2, mode='xy'):
     
     # normalise new unit vectors
-    unit_y = y_new/np.sqrt(np.sum(y_new**2))
-    unit_x = x_new/np.sqrt(np.sum(x_new**2))
-    
-    # find x unit vector normal to y and z
-    unit_z = np.cross(unit_x, unit_y)
+    unit2 = v2/np.sqrt(np.sum(v2**2))
+    unit1 = v1/np.sqrt(np.sum(v1**2))
 
-    A = np.vstack((unit_x, unit_y, unit_z))
+    if mode == 'xy':
+        unit_x = unit1
+        unit_y = unit2
+        unit_z = np.cross(unit_x, unit_y)
+
+    elif mode == 'xz':
+        unit_x = unit1
+        unit_z = unit2
+        unit_y = np.cross(unit_z, unit_x)
+
+    elif mode == 'yz':
+        unit_y = unit1
+        unit_z = unit2
+        unit_x = np.cross(unit_y, unit_z)
+
+    A = np.vstack((unit_x, unit_y, unit_z)).T
 
     return A
 
 
 
 
-
-
-if __name__ == '__main__':
     
-    x = np.array([1,-1,0])
-    y = np.array([1,1,0])
-
-    A = change_basis(x,y)
-    print(A)
+    
 # %%
