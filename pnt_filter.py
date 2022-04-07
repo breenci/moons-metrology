ac7cf569b8b3e426707f16844464f475e09cb52e
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2022-04-07 15:00:37 +0100

    Added pnt_filter.py
    pnt_filter.py filters false detections (e.g. reflections) from the point
    cloud. Points are filtered by size and by there distance from a
    spherical surface (i.e. the focal plane).

---
+++ pnt_filter.py
@@ -0,0 +1,72 @@
+
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+from coord_transform2 import do_transform
+import glob
+import os.path
+
+
+def sphere_filter(data, r, c, tol):
+
+    coords = data[:,1:3]
+    cntrd_coords =  coords - c
+
+    # Calculate how far each point is from sphere surface
+    dist = np.sqrt(np.sum(cntrd_coords**2, axis=1))
+    diff = abs(dist - r)
+
+    # Reject points not on surface to within tol
+    out_coords = coords[diff < tol]
+
+    return out_coords
+
+
+def size_filter(data, size_arr, tol):
+    '''
+    Filters out points not within a tolerance of given sizes
+
+    :param data: numpy array with size values in column 7
+    :param size_arr: 1d numpy array with sizes
+    :param tol: tolerance
+    '''
+
+    # Extract sizes
+    sizes = data[:, 7]
+
+    # Define upper and lower bounds
+    upper_bnds = size_arr + tol
+    lower_bnds = size_arr - tol
+
+    # Create logical mask for first set of bounds then loop through the other
+    # bounds and update the mask with each loop
+    in_bnds_mask = (sizes < upper_bnds[0]) & (sizes > lower_bnds[0])
+
+    for i in range(1, len(upper_bnds)):
+        cond_mask = (sizes < upper_bnds[i]) & (sizes > lower_bnds[i])
+        in_bnds_mask = in_bnds_mask | cond_mask
+
+    # Filter dataset using mask
+    data_out = data[in_bnds_mask]
+
+    return data_out
+
+if __name__ == '__main__':
+    ten_deg = np.loadtxt('caltest_25_03_22/10_deg.txt')
+    
+    coords = ten_deg[:,1:4]
+
+    pln_inds = np.arange(1,17)
+    orgn_ind = 293
+    y0_ind = 7
+
+    ten_deg[:,1:4] = do_transform(coords, pln_inds, orgn_ind, y0_ind)
+
+    fltrd = sphere_filter(ten_deg[:,1:4], 4101.4, (4101.4,0,0), 4)
+    sfiltrd = size_filter(ten_deg, np.array([2.6,1.8,1.1]), .4)
+
+    plt.scatter(fltrd[:,1], fltrd[:,2], s=1)
+    plt.show()
+
+
+# %%
