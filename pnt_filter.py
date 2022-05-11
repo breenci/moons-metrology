a6866fc6eeb40ef48dcaffe91342932961e8ff98
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2022-05-11 14:10:16 +0100

    Add functions to allow filtering directly from file
    Filtering functions now return a mask which defines the filtering. This
    mask can be inputted into new functions which work directly on file. Also
    added some docstrings and comments.

--- pnt_filter.py
+++ pnt_filter.py
@@ -2,35 +2,53 @@
 # %%
 import numpy as np
 import matplotlib.pyplot as plt
-from coord_transform2 import do_transform
-import glob
-import os.path
 
 
-def sphere_filter(data, r, c, tol):
-    """Filters out any points not on a spherical surface to within a tolerance"""
+def sphere_filter(coords, r, c, tol):
+    """
+     Filters out any points not on a spherical surface to within a tolerance
 
-    # extract coords
-    coords = data[:,1:4]
+    :param coords: 3D point cloud coordinates with X, Y, and Z in columns
+    :type data: N x 3 numpy array
+    :param r: Radius of sphere which defines filtering surface
+    :type r: float
+    :param c: Centre of sphere which defines filtering surface
+    :type c: 1 x 3 numpy array
+    :param tol: values within this tolerance are accepted
+    :type tol: float
+    :return: Filtered 3D point cloud coordinates with X, Y, and Z in columns
+    :rtype: N x 3 numpy array
+    """
 
-    # Subtract centre of sphere from coords to satisfy equation of sphere 
-    cntrd_coords =  coords - c
+    # Subtract centre of sphere from coords to satisfy equation of sphere
+    cntrd_coords = coords - c
 
     # Calculate how far each point is from sphere surface
     dist = np.sqrt(np.sum(cntrd_coords**2, axis=1))
     diff = abs(dist - r)
 
     # Reject points not on surface to within tol
-    out_coords = coords[diff < tol]
+    sphere_mask = diff < tol
+    out_coords = coords[sphere_mask]
 
-    return out_coords
+    return sphere_mask, out_coords
 
 
-def size_filter(data, size_arr, tol):
-    """Filters out points not within a tolerance of given sizes"""
+def size_filter(coords, sizes, size_arr, tol):
+    """
+    Filters out points not within a tolerance of given sizes
 
-    # Extract sizes
-    sizes = data[:, 7]
+    :param coords: 3D point cloud coordinates with X, Y, and Z in columns
+    :type data: N x 3 numpy array
+    :param sizes: Array of point sizes
+    :type sizes: 1d numpy array
+    :param size_arr: Array of desired sizes
+    :type size_arr: 1d numpy array
+    :param tol: tolerance
+    :type tol: float
+    :return: Filtered data
+    :rtype: N x 3 numpy array
+    """
 
     # Define upper and lower bounds
     upper_bnds = size_arr + tol
@@ -45,26 +63,68 @@ def size_filter(data, size_arr, tol):
         in_bnds_mask = in_bnds_mask | cond_mask
 
     # Filter dataset using mask
-    data_out = data[in_bnds_mask]
+    data_out = coords[in_bnds_mask]
 
-    return data_out
+    return in_bnds_mask, data_out
+
+
+def sphere_filter_pntcld(fn, r, c, tol):
+    """Apply sphere filter to a moons point cloud file"""
+
+    # load the point cloud file
+    pntcld_data = np.loadtxt(fn)
+
+    # extract coordinates
+    coords = pntcld_data[:, 1:4]
+
+    # get the mask which will filter unwanted values and apply to coords
+    sphere_mask, _ = sphere_filter(coords, r, c, tol)
+    pntcld_fltrd = pntcld_data[sphere_mask]
+
+    return pntcld_fltrd
+
+
+def size_filter_pntcld(fn, size_arr, tol):
+    """Apply sphere filter to a moons point cloud file"""
+
+    # load the point cloud file
+    pntcld_data = np.loadtxt(fn)
+
+    # extract coords and point sizes
+    coords = pntcld_data[:, 1:4]
+    sizes = pntcld_data[:, 7]
+
+    # get the mask which will filter unwanted values and apply to coords
+    size_mask, _ = size_filter(coords, sizes, size_arr, tol)
+    pntcld_fltrd = pntcld_data[size_mask]
+
+    return pntcld_fltrd
 
 
 if __name__ == '__main__':
-    ten_deg = np.loadtxt('caltest_25_03_22/10_deg.txt')
-    
-    coords = ten_deg[:,1:4]
 
-    pln_inds = np.arange(1,17)
-    orgn_ind = 293
-    y0_ind = 7
+    # demonstration of the spherical filter on a test point cloud file
+    # load a test file
+    unfltrd = np.loadtxt('caltest_22_06_21/transformed/uncalibrated/0_deg_t.txt')
 
-    ten_deg[:,1:4] = do_transform(coords, pln_inds, orgn_ind, y0_ind)
+    # do the sphere filtering. Radius of curvature of the plate is 4101.1mm, 
+    # centre is assumed to be directly above the zero point => c = (4101.1,0,0)
+    # tolerance = 4
+    fltrd = sphere_filter_pntcld('caltest_22_06_21/transformed/uncalibrated/0_deg_t.txt',
+                                 4101.1, np.array([4101.1, 0, 0]), 4)
 
-    fltrd = sphere_filter(ten_deg, 4101.4, (4101.4,0,0), 4)
-    sfiltrd = size_filter(ten_deg, np.array([2.6,1.8,1.1]), .4)
-
-    plt.scatter(fltrd[:,1], fltrd[:,2], s=1)
+    # filter removes any points not on the spherical focal plane. Only fpu dots
+    # remain
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.scatter(unfltrd[:, 2], unfltrd[:, 3], s=1)
+    ax1.set_xlabel('Y (mm)')
+    ax1.set_ylabel('Z (mm)')
+    ax1.set_title('Unfiltered')
+    ax2.scatter(fltrd[:, 2], fltrd[:, 3], s=1)
+    ax2.set_xlabel('Y (mm)')
+    ax2.set_ylabel('Z (mm)')
+    ax2.set_title('Filtered')
+    plt.tight_layout()
     plt.show()
 
 
