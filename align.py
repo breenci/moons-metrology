b9ea854a4d9d10aca2e6ea78878f93e3a4d2dca2
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2022-05-23 11:23:19 +0100

    Added comments to kabsch-uneyama function

--- align.py
+++ align.py
@@ -2,26 +2,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from get_corr import get_corr
-from coord_transform2 import plane_fitter, matrix_transform
+from coord_transform2 import matrix_transform
 
 
 def kabsch_umeyama(A, B):
-    assert A.shape == B.shape
+    """Apply the Kabsch Uneyama algorithm"""
+    
+    # see https://web.stanford.edu/class/cs273/refs/umeyama.pdf
     n, m = A.shape
 
+    # get mean vector of A and B
     A_cntr = np.mean(A, axis=0)
     B_cntr = np.mean(B, axis=0)
+
+    # calculate the variance of A
     A_var = np.mean(np.linalg.norm(A - A_cntr, axis=1) ** 2)
 
+    # get the covariance matrix of A and B and do SVD 
     covar_mat = np.matmul((A - A_cntr).T, (B - B_cntr)) / n
     U, D, VT = np.linalg.svd(covar_mat)
+
+    # S = identity matrix if Det(U)*Det(V) = 1
+    # S = Diag(1,...,1,-1) if Det(U)*Det(V) = -1
     d = np.sign(np.linalg.det(U) * np.linalg.det(VT))
     S = np.diag([1] * (m - 1) + [d])
 
+    # define rotation matrix (R), scaling factor (c) and translation (t) 
     R = np.matmul(np.matmul(U,S), VT)
     c = A_var / np.trace(np.diag(D) @ S)
     t = A_cntr - np.matmul(c * R, B_cntr)
 
+    # bring all together to create augmented transformation matrix
     trans_mat = np.eye(4)
     trans_mat[:3,:3] = c * R
     trans_mat[:3,3] = t 
@@ -34,30 +45,43 @@ if __name__ == '__main__':
     # load datasets
     cam_mask = np.loadtxt('mask_test/mask_AC_01.txt')
     met_mask = np.loadtxt('mask_test/transformed_coords_mask1_15092021.txt')
-    met_mask[:,0] = 0
+    #met_mask[:,0] = 0
 
-    # pad and reflect AC camera image in y axis
     cmask_pad = np.hstack((cam_mask, np.zeros((len(cam_mask[:,1]), 1))))
-    cmask_pad[:,1] = -1*cmask_pad[:,1]
 
-    a,b = get_corr(met_mask, cmask_pad, (25,11), (13,0))
+    a,b = get_corr(met_mask, cmask_pad, (25,11), (13,24))
 
     srtd_cmask = cmask_pad[b]
     srtd_mmask = met_mask[a]
 
-    norm,_ = plane_fitter(met_mask)
-
     t_mat = kabsch_umeyama(srtd_mmask, srtd_cmask)
-
-
+    print(t_mat)
     trans_cmask = matrix_transform(srtd_cmask, t_mat)
+
 
     diff = np.abs(srtd_mmask - trans_cmask)
     rms = np.sqrt(np.mean(diff**2, axis=0))
-    fig1, ax = plt.subplots()
-    ax.scatter(np.arange(len(diff[:,1])), diff[:,2])
-    ax.axhline(rms[2])
+    rms_pnts = np.sqrt(np.mean(diff**2, axis=1))
+    
+    fig1, ax1 = plt.subplots()
+    ax1.scatter(np.arange(len(diff[:,1])), diff[:,1], c='r', marker='+', label='Y')
+    ax1.scatter(np.arange(len(diff[:,1])), diff[:,2], c='b', s=10, label='Z')
+    ax1.scatter(np.arange(len(diff[:,1])), diff[:,0], c='g', s=10, label='X')
+    ax1.legend()
+    ax1.axhline(rms[2], c='b')
+    ax1.axhline(rms[1], c='r')
+    ax1.axhline(rms[0], c='g')
+    ax1.set_ylabel('RMS Error (mm)')
+    ax1.set_xlabel('Dot No.')
     plt.show()
 
+    fig2, ax = plt.subplots()
+    scat = ax.scatter(srtd_mmask[:,1], srtd_mmask[:,2], c=rms_pnts)
+    cb = plt.colorbar(scat)
+
+
+# %%
+
+# %%
 
 # %%
