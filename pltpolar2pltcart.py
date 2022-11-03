d879dadc622cea6aae4b069bbf55dc50b6731ade
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2022-11-03 14:27:35 +0000

    Add a function to convert from plate polar to cartesian coordinates

---
+++ pltpolar2pltcart.py
@@ -0,0 +1,75 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def polar2cart(rho, theta, roc, angle_unit='rad'):
+    """Convert from plate polar to plate cartesian coordinate systems
+    
+    :param rho: Radial coordinate of target
+    :type rho: float
+    :param theta: Angular coordinate of target
+    :type theta: float
+    :param roc: Radius of curvature of plate
+    :type roc: float
+    :param angle_unit: unit for input angles. Radians or degrees, defaults to 'rad'
+    :type angle_unit: str, optional
+    :return: Plate cartesian coordinates of target
+    :rtype: tuple
+    """
+
+    # covert to radians if necessary
+    if angle_unit == 'deg':
+        rho_rad = rho * (180/np.pi)
+        theta_rad = theta * (180/np.pi)
+
+    elif angle_unit == 'rad':
+        rho_rad = rho
+        theta_rad = theta
+
+    # define angle phi between centre of curvature and target
+    phi = rho_rad/roc
+
+    # define projection of r onto flat plane
+    r_proj = roc * np.sin(phi)
+
+    # return x,y,z
+    x_plt = roc * (np.cos(phi) - 1)
+    z_plt = r_proj * np.cos(theta_rad)
+    y_plt = r_proj * np.sin(theta_rad)
+
+    return x_plt, y_plt, z_plt
+
+
+if __name__ == '__main__':
+    #ACQ6
+    rho6 = 158.331147 #in mm
+    theta6 = 2.335426 #in rad
+    plt_roc = 4101.8 #in mm
+
+    #ACQ17
+    rho17 = 410.535532
+    theta17 = 5.257187
+
+    #ACQ20
+    rho20 = 392.96983
+    theta20 = 0.688058
+
+    # get x,y,z and plot
+    rhos = np.array([rho6, rho17, rho20])
+    thetas = np.array([theta6, theta17, theta20])
+    labels = np.array(["ACQ6", "ACQ17", "ACQ20"])
+
+    x,y,z = polar2cart(rhos, thetas, plt_roc)
+
+    fig, ax1 = plt.subplots()
+    for i in range(len(x)):
+        ax1.scatter(y[i], z[i], label=labels[i])
+    ax1.scatter(0, 0, label="Origin")
+    ax1.set_ylabel("Y (mm)")
+    ax1.set_xlabel("Z (mm)")
+    plt.legend()
+    plt.show()
+
+
+# %%
