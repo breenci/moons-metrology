2b2f8a817e843efd85796680a96a890e670e5a98
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2023-01-13 13:52:32 +0000

    Linting

--- src/align.py
+++ src/align.py
@@ -7,7 +7,7 @@ from coord_transform2 import matrix_transform
 
 def kabsch_umeyama(A, B, scale=True):
     """Apply the Kabsch Uneyama algorithm"""
-    
+
     # see https://web.stanford.edu/class/cs273/refs/umeyama.pdf
     n, m = A.shape
 
@@ -28,20 +28,20 @@ def kabsch_umeyama(A, B, scale=True):
     S = np.diag([1] * (m - 1) + [d])
 
     # define rotation matrix (R), scaling factor (c) and translation (t) 
-    R = np.matmul(np.matmul(U,S), VT)
+    R = np.matmul(np.matmul(U, S), VT)
     c = A_var / np.trace(np.matmul(np.diag(D), S))
     t = A_cntr - np.matmul(c * R, B_cntr)
 
     # bring all together to create augmented transformation matrix
-    if scale == True:
+    if scale is True:
         trans_mat = np.eye(4)
-        trans_mat[:3,:3] = c * R
-        trans_mat[:3,3] = t
+        trans_mat[:3, :3] = c * R
+        trans_mat[:3, 3] = t
 
-    if scale == False:
+    if scale is False:
         trans_mat = np.eye(4)
-        trans_mat[:3,:3] = R
-        trans_mat[:3,3] = t
+        trans_mat[:3, :3] = R
+        trans_mat[:3, 3] = t
 
     return trans_mat
 
@@ -50,39 +50,39 @@ def align_measurements(ref, unal, targ_ids):
     """Aligns point clouds with reference"""
     # load ref id and xyz
     # get targets from input ids
-    ref_ids = ref[:,0]
+    ref_ids = ref[:, 0]
 
     # load each unaligned data set and align to ref using KU
-    unal_ids = unal[:,0]
-    
+    unal_ids = unal[:, 0]
+
     # Only use reference targets that are detected in both datasets
     # Use ids to find these targets
     common_ids, ref_idxs, unal_idxs = np.intersect1d(ref_ids, unal_ids, 
                                                      return_indices=True)
-    
+
     # Mask to select targets
     common_targs_mask = np.isin(common_ids, targ_ids)
-    
+
     # get position of targets in reference and unaligned datasets
     ref_targs = ref[ref_idxs[common_targs_mask]]
     unal_targs = unal[unal_idxs[common_targs_mask]]
-    
+
     # get transformation matrix for the alignment using KU
-    al_mat = kabsch_umeyama(ref_targs[:,1:4], unal_targs[:,1:4], scale=False)
-    
+    al_mat = kabsch_umeyama(ref_targs[:, 1:4], unal_targs[:, 1:4], scale=False)
+
     # Do alignment and return aligned coords
     al_coords = matrix_transform(unal[:,1:4], al_mat)
-        
+
     return al_coords
 
 
 def align_from_file(ref_fn, unal_fn, targ_ids):
-    
+
     ref_arr = np.loadtxt(ref_fn)
     unal_arr = np.loadtxt(unal_fn)
-    
+
     al_coords = align_measurements(ref_arr, unal_arr, targ_ids)
-    
+
     return al_coords
 
 
