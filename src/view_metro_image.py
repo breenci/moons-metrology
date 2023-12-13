1977942eca0d3bfc2fa1615e772d471500d24414
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2023-12-13 10:55:01 +0000

    Mask brightness threshold with colors
    Add threshold masks with upper and lower bounds. Values beloew threshold
    and above are masked with colors. This allows user to view areas of a
    metrology image which have the appropriate brightness values for targets
    in the metrology system. To be used to help find ideal camera settings.

--- src/view_metro_image.py
+++ src/view_metro_image.py
@@ -1,27 +1,58 @@
 import argparse
 import os
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from PIL import Image
 import sys
+import numpy as np
 
-def plot_grayscale_image(image_path):
-    # Read the image using matplotlib's imread function
-    img = mpimg.imread(image_path)
 
-    # Check if the image is grayscale
-    if len(img.shape) == 3 and img.shape[2] == 3:
-        # Convert the RGB image to grayscale
-        img = img.mean(axis=-1)
+def add_brightness_mask(img_arr, sat_low, sat_high, colors=[[0, 0, 255], [255, 0, 0]]):
+    """Add a brightness mask to a grayscale image array"""
+    
+    # make the image 3 channel
+    arr_3_channel = np.dstack((img_arr, img_arr, img_arr))
+    
+    # mask pixels below brightness threshold
+    sat_mask_low = img_arr < sat_low
+    
+    # color pixels below brightness threshold with first color
+    arr_3_channel[sat_mask_low] = colors[0]
+    
+    # mask pixels above brightness threshold
+    sat_mask_high = img_arr > sat_high
+    
+    # color pixels above brightness threshold with second color
+    arr_3_channel[sat_mask_high] = colors[1]
+    
+    return arr_3_channel
 
+    
+def plot_metro_image(image_path, sat_low=80, sat_high=200):
+    # Read the image and convert to grayscale
+    img = Image.open(image_path).convert('L')
+    
+    # convert to numpy array
+    img_arr = np.array(img)
+    
+    # add brightness mask
+    arr_3_channel = add_brightness_mask(img_arr, sat_low, sat_high)
+    
+    # convert back to PIL image
+    img_3_channel = Image.fromarray(arr_3_channel)
+    
     # Plot the image
-    plt.imshow(img, cmap='gray')
-    plt.title('Grayscale Image')
+    plt.imshow(img_3_channel, cmap='gray')
     plt.axis('off')  # Turn off axis labels
     plt.show()
+
 
 def main():
     parser = argparse.ArgumentParser(description='Plot grayscale image from a JPEG file.')
     parser.add_argument('image_path', help='Path to the grayscale JPEG image file')
+    parser.add_argument('-l', '--low', help='Lower brightness threshold for mask', 
+                        default=80, type=int)
+    parser.add_argument('-u', '--upper', help='Upper brightness threshold for mask', 
+                        default=200, type=int)
 
     args = parser.parse_args()
 
@@ -31,7 +62,7 @@ def main():
         sys.exit(1)
 
     # Plot the grayscale image
-    plot_grayscale_image(args.image_path)
+    plot_metro_image(args.image_path, args.low, args.upper)
 
 if __name__ == "__main__":
     main()
