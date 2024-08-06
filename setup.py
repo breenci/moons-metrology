12bf18f428f88861df7a66b177da47de6e8ae5d7
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2024-08-06 11:47:26 +0100

    Run code as a package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis of MOONS metrology testing data.',
+    author='Ciaran Breen'
+)
